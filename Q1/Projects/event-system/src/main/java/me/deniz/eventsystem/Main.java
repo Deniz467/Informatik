b35dcd3f78d9bb24c039b7297b9a25df0953e94a
Deniz467 <deniz.gebauer11@web.de>
2025-03-05 14:24:14 +0100

    refactor: change Main class to final for better immutability

--- Q1/Projects/event-system/src/main/java/me/deniz/eventsystem/Main.java
+++ Q1/Projects/event-system/src/main/java/me/deniz/eventsystem/Main.java
@@ -15,7 +15,7 @@ import me.deniz.eventsystem.user.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
+public final class Main {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
