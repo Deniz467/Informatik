c7b874684e2f0928947beb439f25fc720420abea
Deniz467 <deniz.gebauer11@web.de>
2025-06-23 06:18:01 +0000

    refactor: update grid initialization and enhance rule application in Conway's Game of Life

--- Q1/Stunden/19_06_25-Conway's Game of Life/ConwaysGameOfLife.java
+++ Q1/Stunden/19_06_25-Conway's Game of Life/ConwaysGameOfLife.java
@@ -3,19 +3,23 @@ import java.util.random.RandomGenerator;
 
 public class ConwaysGameOfLife {
 
-  private static final Slot[][] grid = new Slot[25][100];
+  private static final int HEIGHT = 35;
+  private static final int LENGTH = 100;
+  private static Slot[][] grid = new Slot[HEIGHT][LENGTH];
 
   public static void main(String[] args) throws Exception {
     SecureRandom random = SecureRandom.getInstanceStrong();
     fillGrid(random);
 
     printGrid();
+
     
-    for (int i = 0; i < 100; i++) {
-      applyRules();
+    for (int i = 0; i < 55; i++) {
+      grid = applyRules();
       printGrid();
       Thread.sleep(100L);
     }
+      
       
     
     printGrid();
@@ -27,21 +31,35 @@ public class ConwaysGameOfLife {
      * = new Slot(random.nextBoolean(), x, y); } }
      */
 
-    grid[10][49] = new Slot(true, 10, 49);
-    grid[10][50] = new Slot(true, 10, 50);
-    grid[10][51] = new Slot(true, 10, 51);
-    grid[11][49] = new Slot(true, 11, 49);
-    grid[11][51] = new Slot(true, 11, 51);
-    grid[12][49] = new Slot(true, 12, 49);
-    grid[12][51] = new Slot(true, 12, 51);
-    grid[14][49] = new Slot(true, 14, 49);
-    grid[14][51] = new Slot(true, 14, 51);
-    grid[15][49] = new Slot(true, 15, 49);
-    grid[15][51] = new Slot(true, 15, 51);
-    grid[16][49] = new Slot(true, 16, 49);
-    grid[16][50] = new Slot(true, 16, 50);
-    grid[16][51] = new Slot(true, 16, 51);
+     /*
+     grid[10][49] = new Slot(true, 10, 49);
+     grid[10][50] = new Slot(true, 10, 50);
+     grid[10][51] = new Slot(true, 10, 51);
+     grid[11][49] = new Slot(true, 11, 49);
+     grid[11][51] = new Slot(true, 11, 51);
+     grid[12][49] = new Slot(true, 12, 49);
+     grid[12][51] = new Slot(true, 12, 51);
+     grid[14][49] = new Slot(true, 14, 49);
+     grid[14][51] = new Slot(true, 14, 51);
+     grid[15][49] = new Slot(true, 15, 49);
+     grid[15][51] = new Slot(true, 15, 51);
+     grid[16][49] = new Slot(true, 16, 49);
+     grid[16][50] = new Slot(true, 16, 50);
+     grid[16][51] = new Slot(true, 16, 51);
+     */
+    
+     grid[10][49] = new Slot(true, 10, 49);
+     grid[11][49] = new Slot(true, 11, 49);
+     grid[12][49] = new Slot(true, 12, 49);
+     grid[13][49] = new Slot(true, 13, 49);
+     grid[14][49] = new Slot(true, 14, 49);
+     grid[15][49] = new Slot(true, 15, 49);
+     grid[16][49] = new Slot(true, 16, 49);
+     grid[17][49] = new Slot(true, 17, 49);
+     grid[18][49] = new Slot(true, 18, 49);
+     grid[19][49] = new Slot(true, 19, 49);
 
+  
     for (int x = 0; x < grid.length; x++) {
       for (int y = 0; y < grid[x].length; y++) {
         if (grid[x][y] == null) {
@@ -54,23 +72,23 @@ public class ConwaysGameOfLife {
   }
 
   private static Slot[][] applyRules() {
-    Slot[][] copy = grid.clone();
+    Slot[][] copy = copy();
 
     for (int x = 0; x < grid.length; x++) {
       for (int y = 0; y < grid[x].length; y++) {
-        Slot current = grid[x][y];
+        Slot current = copy[x][y];
         int neighbors = countNeighbors(grid, x, y);
 
         if (current.isAlive()) {
           switch (neighbors) {
-            case 1 -> current.setAlive(false);
+            case 0,1 -> current.setAlive(false);
             case 2, 3 -> {
 
             }
-            case 4 -> current.setAlive(false);
+            case 4, 5, 6, 7, 8 -> current.setAlive(false);
           }
         } else {
-          if (neighbors > 3) {
+          if (neighbors == 3) {
             current.setAlive(true);
           }
         }
@@ -113,6 +131,19 @@ public class ConwaysGameOfLife {
     System.out.println(sb);
   }
 
+  private static Slot[][] copy() {
+    Slot[][] copy = new Slot[HEIGHT][LENGTH];
+
+  for (int x = 0; x < grid.length; x++) {
+    Slot[] height = grid[x];
+    for (int y = 0; y < height.length; y++) {
+      copy[x][y] = grid[x][y].copy();
+    }
+  }
+
+    return copy;
+  }
+
   public static class Slot {
     private boolean alive;
     private int x;
@@ -142,6 +173,10 @@ public class ConwaysGameOfLife {
 
     public int getY() {
       return y;
+    }
+
+    public Slot copy() {
+      return new Slot(alive, x, y);
     }
   }
 
